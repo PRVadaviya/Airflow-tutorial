56f15fe9311a5f4c38fc7ee49673ae4cfe284748
PratikVadaviya <pratikpatel9681@gmail.com>
2026-05-16 19:21:22 +0530

    Ch-3 Operators & DAGs Syncing

--- dags/3_operator.py
+++ dags/3_operator.py
@@ -1,11 +1,12 @@
 from airflow.sdk import dag, task
+from airflow.providers.standard.operators.bash import BashOperator
 
 # create simple dag flow like task1 -> task2 -> task3
 
 @dag(
-     dag_id='first_dag'
+     dag_id='operators_dag'
 )
-def first_dag():
+def operators_dag():
      
      # .python is defined which type of the task perform this function.
      #  many type of the task do airflow like batch processing, pipeline, python function || code
@@ -17,22 +18,24 @@ def first_dag():
      def second_task():
           print('this is the second task')
 
-     @task.python
-     def third_task():
-          print('this is the third task')
+     @task.bash
+     def bash_task_modern():
+          return "echo https://airflow.apache.org/"
+
+     bash_task_oldschool = BashOperator(
+          task_id='bash_task',
+          bash_command='echo https://airflow.apache.org/'
+     )
      
      # define the task dependencies first_task -> second_task -> third_task
      first = first_task() 
      second = second_task()
-     third = third_task()
+     bash_modern = bash_task_modern()
+     bash_oldschool = bash_task_oldschool
 
      # this is the flow of the task excution 
      # make sure that it is defining in the dag level not in the task level
-     first >> second >> third
+     first >> second >> bash_modern >> bash_oldschool
 
 # registering(instantiate) the dag 
-first_dag()
-
-# dags folder is automatically scanned by airflow and which is synced in docker where specific dags are located.
-# so we can see inside logs folder have one folder with dag_processor 
-#    which is responsible for scanning the dags folder and register the dag in airflow.
+operators_dag()
