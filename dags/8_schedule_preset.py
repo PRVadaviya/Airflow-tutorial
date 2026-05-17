7d1efff60f4f70723de7313f73495a7d2598c540
PratikVadaviya <pratikpatel9681@gmail.com>
2026-05-17 10:34:58 +0530

    Ch-8 Scheduling Presets in Airflow

---
+++ dags/8_schedule_preset.py
@@ -0,0 +1,32 @@
+from airflow.sdk import dag, task
+from pendulum import datetime
+# create simple dag flow like task1 -> task2 -> task3
+
+@dag(
+     dag_id='schedule_preset_dag',
+     start_date= datetime(year=2026,month=5,day=1,tz="UTC"),
+     schedule= "@daily",
+     is_paused_upon_creation= False
+)
+def schedule_preset_dag():
+     
+     @task.python
+     def first_task():
+          print('this is the first task')
+
+     @task.python
+     def second_task():
+          print('this is the second task')
+
+     @task.python
+     def third_task():
+          print('this is the third task')
+     
+     # define the task dependencies first_task -> second_task -> third_task
+     first = first_task() 
+     second = second_task()
+     third = third_task()
+
+     first >> second >> third
+
+schedule_preset_dag()
